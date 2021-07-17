f62649b42fff05738f2bfcd05af51c201ff8dba8
RJ Rybarczyk <rj@rybar.tech>
2021-07-17 09:35:15 -0400

    Test stable_checkparams

--- tests/test_pystable.py
+++ tests/test_pystable.py
@@ -10,11 +10,50 @@ class TestPystable(unittest.TestCase):
         self.lib = pystable.load_libstable()
         self.dist_params = self.get_helper('fit.csv')
 
+    def get_helpers(self, file_name: str):
+        '''Retrieves helpers csv file'''
+        file_name = '{}.csv'.format(file_name)
+        path = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.abspath(os.path.join(path, 'helpers'))
+        path = os.path.abspath(os.path.join(path, file_name))
+
+        return pd.read_csv(path)
+
+    def get_fn_params(self, fn_name: str, dist):
+        df_params = self.get_helpers(fn_name)
+        x = []
+        for i in df_params['x']:
+            x.append(i)
+        Nx = len(x)
+        fn_data = [0] * Nx
+        return {
+                'dist': dist,
+                'x': x,
+                'Nx': Nx,
+                'fn': fn_data,
+            }
+
     def get_helper(self, name) -> str:
         base = os.path.dirname(os.path.abspath(__file__))
         base = os.path.join(base, 'helpers')
         base = os.path.join(base, name)
         return pd.read_csv(base).to_dict(orient='records')[0]
+
+    def test_c_stable_checkparams(self):
+        expected_args = (ct.c_double, ct.c_double, ct.c_double, ct.c_double,
+                         ct.c_int)
+        actual = pystable.c_stable_checkparams(self.lib)
+
+        self.assertEqual('stable_checkparams', actual.__name__)
+        self.assertEqual(expected_args, actual.argtypes)
+        self.assertEqual(ct.c_int, actual.restype)
+
+    def test_stable_checkparams(self):
+        actual = pystable.stable_checkparams(self.lib, self.dist_params)
+        self.assertEqual(0, actual)
+
+    def test_stable_checkparams_err(self):
+        pass
 
     def test_c_stable_create(self):
         expected_args = (ct.c_double, ct.c_double, ct.c_double, ct.c_double,
