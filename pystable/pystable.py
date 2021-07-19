34fef957930a7ea1d2c2e9cc93b8cc096818744a
RJ Rybarczyk <rj@rybar.tech>
2021-07-19 15:05:49 -0400

    Define wrapper functions and test

--- pystable/pystable.py
+++ pystable/pystable.py
@@ -28,15 +28,16 @@ def wrap_function(lib: ct.CDLL, funcname: str, restype, argtypes
     return func
 
 
-def create(params: tp.Dict, path=None) -> STABLE_DIST:
+def create(alpha: float, beta: float, sigma: float, mu: float,
+           parameterization: int, path=None) -> STABLE_DIST:
     lib = load_libstable(path)
-    return stable_create(lib, params)
+    return stable_create(lib, alpha, beta, sigma, mu, parameterization)
 
 
-def stable_create(lib: ct.CDLL, params: tp.Dict) -> STABLE_DIST:
+def stable_create(lib: ct.CDLL, alpha: float, beta: float, sigma: float,
+                  mu: float, parameterization: int) -> STABLE_DIST:
     c_fn = c_stable_create(lib)
-    return c_fn(params['alpha'], params['beta'], params['sigma'], params['mu'],
-                params['parameterization'])
+    return c_fn(alpha, beta, sigma, mu, parameterization)
 
 
 def c_stable_create(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
@@ -58,10 +59,16 @@ def c_stable_create(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_create', ret, args)
 
 
-def stable_checkparams(lib: ct.CDLL, params) -> int:
+def checkparams(alpha: float, beta: float, sigma: float, mu: float,
+                parameterization: int, path=None) -> int:
+    lib = load_libstable(path)
+    return stable_checkparams(lib, alpha, beta, sigma, mu, parameterization)
+
+
+def stable_checkparams(lib: ct.CDLL, alpha: float, beta: float, sigma: float,
+                       mu: float, parameterization: int) -> int:
     c_fn = c_stable_checkparams(lib)
-    return c_fn(params['alpha'], params['beta'], params['sigma'], params['mu'],
-                params['parameterization'])
+    return c_fn(alpha, beta, sigma, mu, parameterization)
 
 
 def c_stable_checkparams(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
@@ -70,15 +77,19 @@ def c_stable_checkparams(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_checkparams', ret, args)
 
 
-def stable_cdf(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+def cdf(dist: STABLE_DIST, x: tp.List[float], Nx: int,
+        path=None) -> tp.List[float]:
+    lib = load_libstable(path)
+    return stable_cdf(lib, dist, x, Nx)
+
+
+def stable_cdf(lib: ct.CDLL, dist: STABLE_DIST, x: tp.List[float],
+               Nx: int) -> tp.List[float]:
     c_fn = c_stable_cdf(lib)
-    array_type = ct.c_double * params['Nx']
+    array_type = ct.c_double * Nx
     LP_c_double = ct.POINTER(ct.c_double)
-    cdf = (ct.c_double * params['Nx'])()
-
-    c_fn(params['dist'], array_type(*params['x']), params['Nx'], cdf,
-         LP_c_double())
-
+    cdf = (ct.c_double * Nx)()
+    c_fn(dist, array_type(*x), Nx, cdf, LP_c_double())
     return list(cdf)
 
 
@@ -89,10 +100,15 @@ def c_stable_cdf(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_cdf', ret, args)
 
 
-def stable_cdf_point(lib: ct.CDLL, params: tp.Dict):
+def cdf_point(dist: STABLE_DIST, x: float, path=None):
+    lib = load_libstable(path)
+    return stable_cdf_point(lib, dist, x)
+
+
+def stable_cdf_point(lib: ct.CDLL, dist: STABLE_DIST, x: float):
     c_fn = c_stable_cdf_point(lib)
     LP_c_double = ct.POINTER(ct.c_double)
-    return c_fn(params['dist'], params['x'], LP_c_double())
+    return c_fn(dist, x, LP_c_double())
 
 
 def c_stable_cdf_point(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
@@ -101,10 +117,17 @@ def c_stable_cdf_point(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_cdf_point', ret, args)
 
 
-def stable_fit(lib: ct.CDLL, params: tp.Dict) -> int:
+def fit(dist: STABLE_DIST, data: tp.List[float], length: int,
+        path=None) -> int:
+    lib = load_libstable(path)
+    return stable_fit(lib, dist, data, length)
+
+
+def stable_fit(lib: ct.CDLL, dist: STABLE_DIST, data: tp.List[float],
+               length: int) -> int:
     c_fn = c_stable_fit(lib)
-    array_type = ct.c_double * params['length']
-    return c_fn(params['dist'], array_type(*params['data']), params['length'])
+    array_type = ct.c_double * length
+    return c_fn(dist, array_type(*data), length)
 
 
 def c_stable_fit(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
@@ -113,15 +136,19 @@ def c_stable_fit(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_fit', ret, args)
 
 
-def stable_pdf(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+def pdf(dist: STABLE_DIST, x: tp.List[float], Nx: int,
+        path=None) -> tp.List[float]:
+    lib = load_libstable(path)
+    return stable_pdf(lib, dist, x, Nx)
+
+
+def stable_pdf(lib: ct.CDLL, dist: STABLE_DIST, x: tp.List[float],
+               Nx: int) -> tp.List[float]:
     c_fn = c_stable_pdf(lib)
-    array_type = ct.c_double * params['Nx']
+    array_type = ct.c_double * Nx
     LP_c_double = ct.POINTER(ct.c_double)
-    pdf = (ct.c_double * params['Nx'])()
-
-    c_fn(params['dist'], array_type(*params['x']), params['Nx'], pdf,
-         LP_c_double())
-
+    pdf = (ct.c_double * Nx)()
+    c_fn(dist, array_type(*x), Nx, pdf, LP_c_double())
     return list(pdf)
 
 
@@ -132,16 +159,20 @@ def c_stable_pdf(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_pdf', ret, args)
 
 
-def stable_q(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+def q(dist: STABLE_DIST, q: tp.List[float], Nq: int,
+      path=None) -> tp.List[float]:
+    lib = load_libstable(path)
+    return stable_q(lib, dist, q, Nq)
+
+
+def stable_q(lib: ct.CDLL, dist: STABLE_DIST, q: tp.List[float],
+             Nq: int) -> tp.List[float]:
     c_fn = c_stable_q(lib)
-    array_type = ct.c_double * params['Nq']
+    array_type = ct.c_double * Nq
     LP_c_double = ct.POINTER(ct.c_double)
-    q = (ct.c_double * params['Nq'])()
-
-    c_fn(params['dist'], array_type(*params['q']), params['Nq'], q,
-         LP_c_double())
-
-    return list(q)
+    inv = (ct.c_double * Nq)()
+    c_fn(dist, array_type(*q), Nq, inv, LP_c_double())
+    return list(inv)
 
 
 def c_stable_q(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
@@ -151,13 +182,17 @@ def c_stable_q(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_q', ret, args)
 
 
-def stable_rnd(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+def rnd(dist, rnd: tp.List[float], n: int, path=None) -> tp.List[float]:
+    lib = load_libstable(path)
+    return stable_rnd(lib, dist, rnd, n)
+
+
+def stable_rnd(lib: ct.CDLL, dist, rnd: tp.List[float],
+               n: int) -> tp.List[float]:
     c_fn = c_stable_rnd(lib)
-    array_type = ct.c_double * params['rnd']
-    rnd = (ct.c_double * params['n'])()
-
-    c_fn(params['dist'], array_type(*params['rnd']), params['n'])
-
+    array_type = ct.c_double * rnd
+    rnd = (ct.c_double * n)()
+    c_fn(dist, array_type(*rnd), n)
     return list(rnd)
 
 
