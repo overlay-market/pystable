26dff7155cc781635437251749e718762565acb2
RJ Rybarczyk <rj@rybar.tech>
2021-07-19 11:43:39 -0400

    Test checkparams, create, cdf, pdf

--- pystable/pystable.py
+++ pystable/pystable.py
@@ -29,23 +29,17 @@ def wrap_function(lib: ct.CDLL, funcname: str, restype, argtypes
 
 
 def stable_create(lib: ct.CDLL, params: tp.Dict) -> STABLE_DIST:
-    c_fn = c_stable_create(lib, params)
+    c_fn = c_stable_create(lib)
     return c_fn(params['alpha'], params['beta'], params['sigma'], params['mu'],
                 params['parameterization'])
 
 
-def c_stable_create(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
+def c_stable_create(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     '''
     Call `stable_create` function to create a `StableDist` struct.
 
     Inputs:
       lib    [ct.CDLL]:  libstable dynamically linked library
-      params [Dict]:  `stable_create` input arguments
-        alpha            [double]:
-        beta             [double]:
-        mu               [double]:
-        sigma            [double]:
-        parameterization [int]:
 
     Outputs:
         [Stable_DIST object]: contains resulting `StableDist` struct parameters
@@ -71,27 +65,8 @@ def c_stable_checkparams(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_checkparams', ret, args)
 
 
-def stable_pdf(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
-    c_fn = c_stable_pdf(lib, params)
-    array_type = ct.c_double * params['Nx']
-    LP_c_double = ct.POINTER(ct.c_double)
-    pdf = (ct.c_double * params['Nx'])()
-
-    c_fn(params['dist'], array_type(*params['x']), params['Nx'], pdf,
-         LP_c_double())
-
-    return list(pdf)
-
-
-def c_stable_pdf(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
-    args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint,
-            ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
-    ret = ct.c_void_p
-    return wrap_function(lib, 'stable_pdf', ret, args)
-
-
 def stable_cdf(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
-    c_fn = c_stable_cdf(lib, params)
+    c_fn = c_stable_cdf(lib)
     array_type = ct.c_double * params['Nx']
     LP_c_double = ct.POINTER(ct.c_double)
     cdf = (ct.c_double * params['Nx'])()
@@ -102,7 +77,7 @@ def stable_cdf(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
     return list(cdf)
 
 
-def c_stable_cdf(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
+def c_stable_cdf(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
     args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint,
             ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
     ret = ct.c_void_p
@@ -121,25 +96,6 @@ def c_stable_cdf_point(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
     return wrap_function(lib, 'stable_cdf_point', ret, args)
 
 
-def stable_q(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
-    c_fn = c_stable_q(lib, params)
-    array_type = ct.c_double * params['Nq']
-    LP_c_double = ct.POINTER(ct.c_double)
-    inv = (ct.c_double * params['Nq'])()
-
-    c_fn(params['dist'], array_type(*params['q']), params['Nq'], inv,
-         LP_c_double())
-
-    return list(inv)
-
-
-def c_stable_q(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
-    args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint,
-            ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
-    ret = ct.c_void_p
-    return wrap_function(lib, 'stable_q', ret, args)
-
-
 def stable_fit(lib: ct.CDLL, params: tp.Dict) -> int:
     c_fn = c_stable_fit(lib, params)
     array_type = ct.c_double * params['length']
@@ -150,3 +106,57 @@ def c_stable_fit(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
     args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint)
     ret = ct.c_int
     return wrap_function(lib, 'stable_fit', ret, args)
+
+
+def stable_pdf(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+    c_fn = c_stable_pdf(lib)
+    array_type = ct.c_double * params['Nx']
+    LP_c_double = ct.POINTER(ct.c_double)
+    pdf = (ct.c_double * params['Nx'])()
+
+    c_fn(params['dist'], array_type(*params['x']), params['Nx'], pdf,
+         LP_c_double())
+
+    return list(pdf)
+
+
+def c_stable_pdf(lib: ct.CDLL) -> ct.CDLL._FuncPtr:
+    args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint,
+            ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
+    ret = ct.c_void_p
+    return wrap_function(lib, 'stable_pdf', ret, args)
+
+
+def stable_q(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+    c_fn = c_stable_q(lib, params)
+    array_type = ct.c_double * params['Nq']
+    LP_c_double = ct.POINTER(ct.c_double)
+    q = (ct.c_double * params['Nq'])()
+
+    c_fn(params['dist'], array_type(*params['q']), params['Nq'], q,
+         LP_c_double())
+
+    return list(q)
+
+
+def c_stable_q(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
+    args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint,
+            ct.POINTER(ct.c_double), ct.POINTER(ct.c_double))
+    ret = ct.c_void_p
+    return wrap_function(lib, 'stable_q', ret, args)
+
+
+def stable_rnd(lib: ct.CDLL, params: tp.Dict) -> tp.List[float]:
+    c_fn = c_stable_rnd(lib, params)
+    array_type = ct.c_double * params['rnd']
+    rnd = (ct.c_double * params['n'])()
+
+    c_fn(params['dist'], array_type(*params['rnd']), params['n'])
+
+    return list(rnd)
+
+
+def c_stable_rnd(lib: ct.CDLL, params: tp.Dict) -> ct.CDLL._FuncPtr:
+    args = (ct.POINTER(STABLE_DIST), ct.POINTER(ct.c_double), ct.c_uint)
+    ret = ct.c_void_p
+    return wrap_function(lib, 'stable_rnd', ret, args)
--- tests/test_pystable.py
+++ tests/test_pystable.py
@@ -1,187 +1,121 @@
 import os
-import pytest
 import ctypes as ct
-import typing as tp
 import pandas as pd
 import numpy as np
-import time
-
+import unittest
 import pystable
 
 
-def create_stable(clib: ct.CDLL, params: tp.Dict[str, float]):
-    return pystable.stable_create(clib, params)
+class TestPystable(unittest.TestCase):
 
+    def get_fit(self):
+        '''Get fit dist parameter'''
+        base = os.path.dirname(os.path.abspath(__file__))
+        base = os.path.join(base, 'helpers/fit.csv')
+        return pd.read_csv(base).to_dict(orient='records')[0]
 
-@pytest.fixture
-def lib() -> ct.CDLL:
-    """
-    Fixture to get C library
-    """
-    return pystable.load_libstable()
+    def test_stable_checkparams(self):
+        '''
+        Test `stable_checkparams` high-level function.
+        `stable_checkparams` returns 0 on success.
+        '''
+        lib = pystable.load_libstable()
+        params = self.get_fit()
+        actual = pystable.stable_checkparams(lib, params)
+        self.assertEqual(0, actual)
 
+    def test_c_stable_checkparams(self):
+        '''Test `stable_checkparams` low-level function'''
+        lib = pystable.load_libstable()
+        expected_args = (ct.c_double, ct.c_double, ct.c_double, ct.c_double,
+                         ct.c_int)
+        actual = pystable.c_stable_checkparams(lib)
 
-@pytest.fixture
-def data() -> tp.List[float]:
-    """
-    Fixture to get example dataset
-    """
-    base = os.path.dirname(os.path.abspath(__file__))
-    base = os.path.join(base, 'helpers')
-    base = os.path.join(base, 'data.csv')
-    return pd.read_csv(base)['value'].to_numpy().tolist()
+        self.assertEqual('stable_checkparams', actual.__name__)
+        self.assertEqual(expected_args, actual.argtypes)
+        self.assertEqual(ct.c_int, actual.restype)
 
+    def test_stable_checkparams_err(self):
+        pass
 
-@pytest.fixture
-def fit() -> tp.Dict[str, float]:
-    """
-    Fixture to get stable distribution example params.
-    Should fit the example dataset.
-    """
-    base = os.path.dirname(os.path.abspath(__file__))
-    base = os.path.join(base, 'helpers')
-    base = os.path.join(base, 'fit.csv')
-    return pd.read_csv(base).to_dict(orient='records')[0]
+    def test_c_stable_create(self):
+        '''Test `stable_cdf` low-level function'''
+        lib = pystable.load_libstable()
+        expected_args = (ct.c_double, ct.c_double, ct.c_double, ct.c_double,
+                         ct.c_int)
+        expected_res = ct.POINTER(pystable.STABLE_DIST)
+        actual = pystable.c_stable_create(lib)
 
+        self.assertEqual('stable_create', actual.__name__)
+        self.assertEqual(expected_args, actual.argtypes)
+        self.assertEqual(expected_res, actual.restype)
 
-@pytest.fixture
-def cdfs() -> pd.DataFrame:
-    """
-    Fixture to get generated cdf values from example stable distribution
-    """
-    base = os.path.dirname(os.path.abspath(__file__))
-    base = os.path.join(base, 'helpers')
-    base = os.path.join(base, 'cdfs.csv')
-    return pd.read_csv(base)
+    def test_stable_cdf(self):
+        '''Test `stable_cdf` high-level function'''
+        lib = pystable.load_libstable()
 
+        base = os.path.dirname(os.path.abspath(__file__))
+        base = os.path.join(base, 'helpers/fit.csv')
+        fit = pd.read_csv(base).to_dict(orient='records')[0]
+        dist = pystable.stable_create(lib, fit)
 
-@pytest.fixture
-def pdfs() -> pd.DataFrame:
-    """
-    Fixture to get generated pdf values from example stable distribution
-    """
-    base = os.path.dirname(os.path.abspath(__file__))
-    base = os.path.join(base, 'helpers')
-    base = os.path.join(base, 'pdfs.csv')
-    return pd.read_csv(base)
+        base = os.path.dirname(os.path.abspath(__file__))
+        base = os.path.join(base, 'helpers')
+        base = os.path.join(base, 'cdfs.csv')
+        cdfs = pd.read_csv(base)
+        expected = cdfs['value'].tolist()
 
+        x = cdfs['x'].to_numpy().tolist()
+        Nx = len(x)
+        cdf_params = {'dist': dist, 'x': x, 'Nx': Nx}
 
-@pytest.fixture
-def quantiles() -> pd.DataFrame:
-    """
-    Fixture to get generated cdf^{-1} values from example stable distribution
-    """
-    base = os.path.dirname(os.path.abspath(__file__))
-    base = os.path.join(base, 'helpers')
-    base = os.path.join(base, 'quantiles.csv')
-    return pd.read_csv(base)
+        actual = pystable.stable_cdf(lib, cdf_params)
+        np.testing.assert_allclose(expected, actual, rtol=1e-08)
 
+    def test_c_stable_cdf(self):
+        '''Test `stable_cdf` low-level function'''
+        lib = pystable.load_libstable()
+        actual = pystable.c_stable_cdf(lib)
+        self.assertEqual('stable_cdf', actual.__name__)
 
-# Low level wrapper tests
+    def test_stable_create(self):
+        '''Test `stable_cdf` high-level function'''
+        lib = pystable.load_libstable()
+        params = self.get_fit()
+        expected = {
+                'alpha': 1.3278285879842862,
+                'beta': 0.0816835526225623,
+                'sigma': 0.0006409442772706,
+                'mu_0': -0.00011779403879886721,
+                'mu_1': -2.52748167384e-05
+                }
+        actual = pystable.stable_create(lib, params)
 
-def test_stable_create(lib, fit):
-    """
-    Tests creation of stable distribution
-    """
-    dist = create_stable(lib, fit)
-    # TODO: assert type(dist) == pystable.STABLE_DIST ... diff bw <class 'pystable.pystable.LP_STABLE_DIST'> == <class 'pystable.pystable.STABLE_DIST'>?
+        self.assertEqual(expected['alpha'], actual.contents.alpha)
+        self.assertEqual(expected['beta'], actual.contents.beta)
+        self.assertEqual(expected['sigma'], actual.contents.sigma)
+        self.assertEqual(expected['mu_0'], actual.contents.mu_0)
+        self.assertEqual(expected['mu_1'], actual.contents.mu_1)
 
-    assert fit['alpha'] == dist.contents.alpha
-    assert fit['beta'] == dist.contents.beta
-    assert fit['sigma'] == dist.contents.sigma
+    def test_stable_pdf(self):
+        '''Test `stable_pdf` high-level function'''
+        lib = pystable.load_libstable()
 
-    if fit['parameterization'] == 1:
-        assert fit['mu'] == dist.contents.mu_1
-    else:
-        assert fit['mu'] == dist.contents.mu_0
+        base = os.path.dirname(os.path.abspath(__file__))
+        base = os.path.join(base, 'helpers')
+        base = os.path.join(base, 'pdfs.csv')
+        pdfs = pd.read_csv(base)
 
+        expected = pdfs['value'].tolist()
 
-def test_stable_cdf(lib, fit, cdfs):
-    """
-    Tests cdf values for stable example
-    """
-    expected = cdfs['value'].tolist()
+        x = pdfs['x'].tolist()
+        dist = pystable.stable_create(lib, self.get_fit())
+        params = {'dist': dist, 'x': x, 'Nx': len(x)}
+        actual = pystable.stable_pdf(lib, params)
+        np.testing.assert_allclose(expected, actual, rtol=1e-06)
 
-    dist = create_stable(lib, fit)
-    x = cdfs['x'].to_numpy().tolist()
-    Nx = len(x)
-    cdf = [0] * Nx
-    cdf_params = {
-        'dist': dist,
-        'x': x,
-        'Nx': Nx,
-        'cdf': cdf
-    }
-
-    actual = pystable.stable_cdf(lib, cdf_params)
-    np.testing.assert_allclose(expected, actual, rtol=1e-05)
-
-
-def test_stable_pdf(lib, fit, pdfs):
-    """
-    Tests cdf values for stable example
-    """
-    expected = pdfs['value'].tolist()
-
-    dist = create_stable(lib, fit)
-    x = pdfs['x'].to_numpy().tolist()
-    Nx = len(x)
-    pdf = [0] * Nx
-    pdf_params = {
-        'dist': dist,
-        'x': x,
-        'Nx': Nx,
-        'pdf': pdf
-    }
-
-    actual = pystable.stable_pdf(lib, pdf_params)
-    np.testing.assert_allclose(expected, actual, rtol=1e-05)
-
-
-def test_stable_q(lib, fit, quantiles):
-    """
-    Tests inverse cdf values for stable example
-    """
-    expected = quantiles['value'].tolist()
-
-    dist = create_stable(lib, fit)
-    q = quantiles['q'].to_numpy().tolist()
-    Nq = len(q)
-    inv = [0] * Nq
-    quantile_params = {
-        'dist': dist,
-        'q': q,
-        'Nq': Nq,
-        'inv': inv
-    }
-
-    actual = pystable.stable_q(lib, quantile_params)
-    np.testing.assert_allclose(expected, actual, rtol=1e-05)
-
-
-#  def test_stable_fit(lib, fit, data):
-#      """
-#      Tests fit values for stable example
-#      """
-#      expected = [fit['alpha'], fit['beta'], fit['sigma'], 0, fit['mu']]
-#
-#      init_fit = {
-#          'alpha': 2,
-#          'beta': 0,
-#          'sigma': 1,
-#          'mu': 0,
-#          'parameterization': 1
-#      }
-#      dist = create_stable(lib, init_fit)
-#
-#      length = len(data)
-#      fit_params = {
-#          'dist': dist,
-#          'data': data,
-#          'length': length,
-#      }
-#      status = pystable.stable_fit(lib, fit_params)
-#      assert status == 0  # 0 == finished
-#      actual = [dist.contents.alpha, dist.contents.beta,
-#                dist.contents.sigma, dist.contents.mu_0, dist.contents.mu_1]
-#      np.testing.assert_allclose(expected, actual, rtol=1e-05)
+    def test_c_stable_pdf(self):
+        '''Test `stable_pdf` low-level function'''
+        lib = pystable.load_libstable()
+        actual = pystable.c_stable_pdf(lib)
+        self.assertEqual('stable_pdf', actual.__name__)
