da7de485b0d3d7a504d140816a955b8da2951cd8
Michael Feldman <23126274+mikeyrf@users.noreply.github.com>
2021-07-21 10:10:45 -0400

    fix build.py (#15)

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ import typing as tp
 
 
 # NOTE: Needed for poetry but don't need to implement since setup.py exists
-def build(setup_kwargs: tp.Dict[str, Any]) -> None:
+def build(setup_kwargs: tp.Dict[str, tp.Any]) -> None:
     pass
